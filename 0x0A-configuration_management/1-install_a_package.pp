c5d20709abf8c8e4cb493e4b639e6de218a821ae
Temesgen dora <tdemeke36@gmail.com>
2024-02-24 11:00:57 -0800

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # installing flask through puppet
 
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip', 'wezaoruazgf']
+  require  => Package['python3-pip'],
 }
