63c02a244fbb1e9980616d53fd289dc21ffa47e0
Temesgen dora <tdemeke36@gmail.com>
2024-03-25 13:26:58 -0700

    api: task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+if __name__ == "__main__":
+    import requests
+    import json
+    values = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    done = 0
+    total = 0
+    dump_dict = {}
+
+    for value in values:
+        if value['userId'] == id:
+            total += 1
+            if value['completed']:
+                done += 1
+
+    for user in users:
+        id = user['id']
+        dump_dict[id] = []
+        if user['id'] == id:
+            for value in values:
+                dump_dict[id].append([{"username": user['username'],
+                                       "task": value['title'],
+                                       "completed": value['completed']}])
+    with open("all.json", "a+") as jsonfile:
+        json.dump(dump_dict, fp=jsonfile)
