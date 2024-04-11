c168bea0f11515756e2e182ceda0437833fd78e5
Temesgen dora <tdemeke36@gmail.com>
2024-04-11 10:00:13 -0700

    fixed: task 0 reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    _summary_
+
+    Args:
+        subreddit (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
