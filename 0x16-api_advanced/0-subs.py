56801a2be8753a3705d83e4181d395c650db9b31
Temesgen dora <tdemeke36@gmail.com>
2024-04-11 10:26:41 -0700

    fixed #2: task 0 reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,7 +18,8 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        res = response.json()
+        return res['data']['subscribers']
+    else:
         return 0
-    res = response.json()
-    return res['data']['subscribers']
